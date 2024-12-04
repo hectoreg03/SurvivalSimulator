b1dbbbbcaddb259cda30c5f9dc8d4d746a14c9f1
Hector Manuel Eguiarte Carlos <0243795@up.edu.mx>
2024-12-03 19:21:33 -0600

    Change of textures and materials, change of final text.

--- Assets/Scripts/Other/RoundManager.cs
+++ Assets/Scripts/Other/RoundManager.cs
@@ -8,11 +8,13 @@ using UnityEngine.SceneManagement;
 public class RoundManager : MonoBehaviour
 {
     //=========== Unity Inspector ============
-    [Header("Requierements")] 
+    [Header("Requirements")]
     [SerializeField] private ZombieSpawner spawner;
     [SerializeField] private TMP_Text roundIndicator;
-    
-    [Header("Parameters")] 
+    [SerializeField] private TMP_Text winMessage; // Reference to display "You Win"
+    [SerializeField] private bool continuePlaying; // Reference to display "You Win"
+
+    [Header("Parameters")]
     [Range(1, 50)]
     [SerializeField] private int zombieAmount;
     //========================================
@@ -20,22 +22,40 @@ public class RoundManager : MonoBehaviour
     private int round;
 
     public void Start()
-    {
+    { 
+        continuePlaying = true;
         round = 0;
         NewRound();
+        winMessage.gameObject.SetActive(false); // Ensure the win message is hidden initially
     }
 
     public void FixedUpdate()
     {
-        if(GameObject.FindGameObjectsWithTag("Zombie").Length < 1) NewRound();
+        if (GameObject.FindGameObjectsWithTag("Zombie").Length < 1&&continuePlaying==true) NewRound();
     }
-    
+
     public void NewRound()
     {
         round++;
         roundIndicator.SetText(round.ToString());
-        spawner.SpawnZombies(zombieAmount);
-        zombieAmount *= (int)1.25;
+
+        if (round == 6)
+        {
+            StartCoroutine(HandleWin());
+            continuePlaying = false;
+        }
+        else
+        {
+            spawner.SpawnZombies(zombieAmount);
+            zombieAmount = Mathf.CeilToInt(zombieAmount * 1.25f);
+            roundIndicator.SetText("You Won");
+        }
+    }
+
+    private IEnumerator HandleWin()
+    {
+        yield return new WaitForSeconds(3f); // Wait for 3 seconds
+        SceneManager.LoadScene("Menu"); // Go to main menu
     }
 
     public void EndGame()
